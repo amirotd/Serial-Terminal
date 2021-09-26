6a3a44c51ab6377afad8eab936d3cd004aed783c
amir otd <amir.otd.1@gmail.com>
2021-09-26 17:58:07 +0200

    whole structure changed to oop

--- serialGUI.py
+++ serialGUI.py
@@ -28,190 +28,187 @@ _stop_thread = True
 ser = serial.Serial()
 
 
-def open_port():
-    try:
-        ser.port = portName_var.get()
-        ser.baudrate = baudVar.get()
-        ser.bytesize = _dataSize[dataVar.get()]
-        ser.parity = _parity[parityVar.get()]
-        ser.stopbits = _stopBit[stopBitVar.get()]
+class MySerialWindow(tk.Tk):
 
-        if handShakeVar.get() == 'RTS/CTS':
-            ser.rtscts = 1
-            ser.xonxoff = False
-        elif handShakeVar.get() == 'Xon/Xoff':
-            ser.xonxoff = 1
-            ser.rtscts = False
-        else:
-            ser.xonxoff = False
-            ser.rtscts = False
+    def __init__(self, *args, **kwargs):
 
-        ser.open()
-        open_label_1 = tk.Label(serial_frame, image=close_photo)
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # ____________________tkinter initial window configuration
+
+        self.title("Serial Terminal")
+        self.geometry('1000x425+200+100')
+        self['padx'] = 20
+        self['pady'] = 20
+
+        # ____________________column configuration
+        self.grid_columnconfigure(0, weight=10)
+        self.grid_columnconfigure(1, weight=1)
+
+        # ____________________row configuration
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=15)
+
+        # ____________________receive data box configuration
+        self.receive_box = tk.Text(self, height=8)
+        self.receive_box.grid(column=0, row=1, sticky='nsew', padx=(0, 10))
+        self.scroll = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.receive_box.yview)
+        self.scroll.grid(column=0, row=1, sticky='nse')
+        self.receive_box["yscrollcommand"] = self.scroll.set
+
+        # ____________________send data frame configuration
+        self.send_frame = tk.LabelFrame(self, text='Send Data')
+        self.send_frame.grid(column=0, row=0, sticky='wens', padx=(0, 10), pady=(0, 10))
+        self.send_frame['padx'] = 10
+        self.send_box = tk.Text(self.send_frame, height=1)
+        self.send_box.grid(column=0, row=0, padx=(10, 5))
+        self.send_button = tk.Button(self.send_frame, text='Send', width=7, command=self.send_data)
+        self.send_button.grid(column=1, row=0, padx=(10, 10))
+
+        # ____________________serial settings box configuration
+        self.serial_frame = tk.LabelFrame(self, text='Serial Port Settings')
+        self.serial_frame.grid(column=1, row=0, rowspan=2, sticky='esn', padx=(10, 5))
+
+        self.portName_var = tk.StringVar(self)
+        self.portName_frame = tk.LabelFrame(self.serial_frame, text='Port Name')
+        self.portName_box = tk.Entry(self.portName_frame, textvariable=self.portName_var)
+        self.portName_frame.grid(column=0, row=0)
+        self.portName_box.grid(column=0, row=0)
+
+        self.baudVar = tk.IntVar(self)
+        self.baudVar.set(9600)
+        self.baudrate_frame = tk.LabelFrame(self.serial_frame, text='Baudrate')
+        self.baudrate_frame.grid(column=0, row=1)
+        self.baudrate_box = ttk.Combobox(self.baudrate_frame, text=self.baudVar, value=self.baudVar)
+        self.baudrate_box.grid(column=0, row=0)
+        self.baudrate_box['values'] = _baudrate_choices
+
+        self.dataVar = tk.IntVar(self)
+        self.dataVar.set(8)
+        self.dataSize_frame = tk.LabelFrame(self.serial_frame, text='Data Size')
+        self.dataSize_frame.grid(column=0, row=2)
+        self.dataSize_box = ttk.Combobox(self.dataSize_frame, text=self.dataVar, value=self.dataVar)
+        self.dataSize_box.grid(column=0, row=0)
+        self.dataSize_box['values'] = list(_dataSize.keys())
+
+        self.parityVar = tk.StringVar(self)
+        self.parityVar.set('None')
+        self.parity_frame = tk.LabelFrame(self.serial_frame, text='Parity')
+        self.parity_frame.grid(column=0, row=3)
+        self.parity_box = ttk.Combobox(self.parity_frame, text=self.parityVar, value=self.parityVar)
+        self.parity_box.grid(column=0, row=0)
+        self.parity_box['values'] = list(_parity.keys())
+
+        self.stopBitVar = tk.StringVar(self)
+        self.stopBitVar.set('One')
+        self.stopBit_frame = tk.LabelFrame(self.serial_frame, text='Stop Bit')
+        self.stopBit_frame.grid(column=0, row=4)
+        self.stopBit_box = ttk.Combobox(self.stopBit_frame, text=self.stopBitVar, value=self.stopBitVar)
+        self.stopBit_box.grid(column=0, row=0)
+        self.stopBit_box['values'] = list(_stopBit.keys())
+
+        self.handShakeVar = tk.StringVar(self)
+        self.handShakeVar.set('None')
+        self.handShake_frame = tk.LabelFrame(self.serial_frame, text='HandShake')
+        self.handShake_frame.grid(column=0, row=5)
+        self.handShake_box = ttk.Combobox(self.handShake_frame, text=self.handShakeVar, value=self.handShakeVar)
+        self.handShake_box.grid(column=0, row=0)
+        self.handShake_box['values'] = _handShake_choices
+
+        # ____________________button for opening the serial port
+        self.open_photo = tk.PhotoImage(file='open.png')
+        self.close_photo = tk.PhotoImage(file='close.png')
+        self.open_label = tk.Label(self.serial_frame, image=self.open_photo)
+        self.open_label.grid(column=0, row=6, sticky='w', padx=(10, 0), pady=(10, 0))
+        self.open_button = tk.Button(self.serial_frame, text='Open Port', command=self.open_port, width=10)
+        self.open_button.grid(column=0, row=6, pady=(10, 0), padx=(40, 0))
+
+        # ____________________label and button for clearing the receive data box details
+        self.clear_photo = tk.PhotoImage(file='clear2.png')
+        self.clear_Label = tk.Label(self.serial_frame, image=self.clear_photo)
+        self.clear_Label.grid(column=0, row=7, sticky='w', padx=(10, 0), pady=(5, 0))
+        self.clear_button = tk.Button(self.serial_frame, text='Clear Output',
+                                      command=lambda: self.receive_box.delete('1.0', tk.END))
+        self.clear_button.grid(column=0, row=7, padx=(40, 0), pady=(5, 0))
+
+        # ____________________ABOUT message box button
+        self.about_photo = tk.PhotoImage(file='about.png')
+        self.about_label = tk.Label(self.serial_frame, image=self.about_photo)
+        self.about_label.grid(column=0, row=8, sticky='w', padx=(10, 0), pady=(5, 0))
+        self.about_button = tk.Button(self.serial_frame, text='ABOUT', foreground='blue', width=10,
+                                      command=self.show_info)
+        self.about_button.grid(column=0, row=8, pady=(5, 0), padx=(40, 0))
+
+    def open_port(self):
+        try:
+            ser.port = self.portName_var.get()
+            ser.baudrate = self.baudVar.get()
+            ser.bytesize = _dataSize[self.dataVar.get()]
+            ser.parity = _parity[self.parityVar.get()]
+            ser.stopbits = _stopBit[self.stopBitVar.get()]
+
+            if self.handShakeVar.get() == 'RTS/CTS':
+                ser.rtscts = 1
+                ser.xonxoff = False
+            elif self.handShakeVar.get() == 'Xon/Xoff':
+                ser.xonxoff = 1
+                ser.rtscts = False
+            else:
+                ser.xonxoff = False
+                ser.rtscts = False
+
+            ser.open()
+            open_label_1 = tk.Label(self.serial_frame, image=self.close_photo)
+            open_label_1.grid(column=0, row=6, sticky='w', padx=(10, 0), pady=(10, 0))
+            open_button_1 = tk.Button(self.serial_frame, text='Close Port', command=self.close_port, width=10)
+            open_button_1.grid(column=0, row=6, pady=(10, 0), padx=(40, 0))
+
+            try:
+                task.start()
+            except RuntimeError:
+                global _stop_thread
+                _stop_thread = True
+
+        except serial.serialutil.SerialException:
+            print("Please Enter a Valid Port Name!!")
+            messagebox.showerror("Error", "Please Enter a Valid Port Name!!")
+
+    def print_result(self):
+        while True:
+            global _stop_thread
+            while _stop_thread:
+                self.receive_box.insert(tk.END, ser.read())
+                self.receive_box.see(tk.END)
+                print(ser.baudrate, ser.bytesize, ser.parity, ser.stopbits,
+                      ser.rtscts, ser.xonxoff, ser.is_open is False)
+
+    def close_port(self):
+        global _stop_thread
+        _stop_thread = False
+        ser.close()
+
+        open_label_1 = tk.Label(self.serial_frame, image=self.open_photo)
         open_label_1.grid(column=0, row=6, sticky='w', padx=(10, 0), pady=(10, 0))
-        open_button_1 = tk.Button(serial_frame, text='Close Port', command=close_port, width=10)
+        open_button_1 = tk.Button(self.serial_frame, text='Open Port', command=self.open_port, width=10)
         open_button_1.grid(column=0, row=6, pady=(10, 0), padx=(40, 0))
 
+    def send_data(self):
         try:
-            task.start()
-        except RuntimeError:
-            global _stop_thread
-            _stop_thread = True
+            temp_send_data = self.send_box.get("1.0", "end-1c")
+            ser.write(temp_send_data.encode('utf-8'))
+            print(temp_send_data)
+        except serial.serialutil.SerialException:
+            print("First Open a Port!!!")
+            messagebox.showerror("Error", "First Open a Port!!!")
 
-    except serial.serialutil.SerialException:
-        print("Please Enter a Valid Port Name!!")
-        messagebox.showerror("Error", "Please Enter a Valid Port Name!!")
+    def show_info(self):
+        print(":)))))")
+        messagebox.showinfo("About", "This program is written in python and\n it's"
+                                     " using pyserial library and tkinter GUI")
 
-
-def print_result():
-    while True:
-        global _stop_thread
-        while _stop_thread:
-            receive_box.insert(tk.END, ser.read())
-            receive_box.see(tk.END)
-            print(ser.baudrate, ser.bytesize, ser.parity, ser.stopbits,
-                  ser.rtscts, ser.xonxoff, ser.is_open is False)
-
-
-def close_port():
-    global _stop_thread
-    _stop_thread = False
-    ser.close()
-
-    open_label_1 = tk.Label(serial_frame, image=open_photo)
-    open_label_1.grid(column=0, row=6, sticky='w', padx=(10, 0), pady=(10, 0))
-    open_button_1 = tk.Button(serial_frame, text='Open Port', command=open_port, width=10)
-    open_button_1.grid(column=0, row=6, pady=(10, 0), padx=(40, 0))
-
-
-def send_data():
-    try:
-        temp_send_data = send_box.get("1.0", "end-1c")
-        ser.write(temp_send_data.encode('utf-8'))
-        print(temp_send_data)
-    except serial.serialutil.SerialException:
-        print("First Open a Port!!!")
-        messagebox.showerror("Error", "First Open a Port!!!")
-
-
-def show_info():
-    print(":)))))")
-    messagebox.showinfo("About", "This program is written in python and\n it's"
-                                 " using pyserial library and tkinter GUI")
-
-
-def run():
-    window.mainloop()
-
-
-# ____________________tkinter initial window configuration
-window = tk.Tk()
-
-window.title("Serial Terminal")
-window.geometry('1000x425+200+100')
-window['padx'] = 20
-window['pady'] = 20
-
-# ____________________column configuration
-window.grid_columnconfigure(0, weight=10)
-window.grid_columnconfigure(1, weight=1)
-
-# ____________________row configuration
-window.grid_rowconfigure(0, weight=1)
-window.grid_rowconfigure(1, weight=15)
-
-# ____________________receive data box configuration
-receive_box = tk.Text(window, height=8)
-receive_box.grid(column=0, row=1, sticky='nsew', padx=(0, 10))
-scroll = tk.Scrollbar(window, orient=tk.VERTICAL, command=receive_box.yview)
-scroll.grid(column=0, row=1, sticky='nse')
-receive_box["yscrollcommand"] = scroll.set
-
-# ____________________send data frame configuration
-send_frame = tk.LabelFrame(window, text='Send Data')
-send_frame.grid(column=0, row=0, sticky='wens', padx=(0, 10), pady=(0, 10))
-send_frame['padx'] = 10
-# sendBox_label = tk.Label(send_frame, text='Send Data:')
-# sendBox_label.grid(column=0, row=0)
-send_box = tk.Text(send_frame, height=1)
-send_box.grid(column=0, row=0, padx=(10, 5))
-send_button = tk.Button(send_frame, text='Send', width=7, command=send_data)
-send_button.grid(column=1, row=0, padx=(10, 10))
-
-# ____________________serial settings box configuration
-serial_frame = tk.LabelFrame(window, text='Serial Port Settings')
-serial_frame.grid(column=1, row=0, rowspan=2, sticky='esn', padx=(10, 5))
-
-portName_var = tk.StringVar(window)
-portName_frame = tk.LabelFrame(serial_frame, text='Port Name')
-portName_box = tk.Entry(portName_frame, textvariable=portName_var)
-portName_frame.grid(column=0, row=0)
-portName_box.grid(column=0, row=0)
-
-baudVar = tk.IntVar(window)
-baudVar.set(9600)
-baudrate_frame = tk.LabelFrame(serial_frame, text='Baudrate')
-baudrate_frame.grid(column=0, row=1)
-baudrate_box = ttk.Combobox(baudrate_frame, text=baudVar, value=baudVar)
-baudrate_box.grid(column=0, row=0)
-baudrate_box['values'] = _baudrate_choices
-
-dataVar = tk.IntVar(window)
-dataVar.set(8)
-dataSize_frame = tk.LabelFrame(serial_frame, text='Data Size')
-dataSize_frame.grid(column=0, row=2)
-dataSize_box = ttk.Combobox(dataSize_frame, text=dataVar, value=dataVar)
-dataSize_box.grid(column=0, row=0)
-dataSize_box['values'] = list(_dataSize.keys())
-
-parityVar = tk.StringVar(window)
-parityVar.set('None')
-parity_frame = tk.LabelFrame(serial_frame, text='Parity')
-parity_frame.grid(column=0, row=3)
-parity_box = ttk.Combobox(parity_frame, text=parityVar, value=parityVar)
-parity_box.grid(column=0, row=0)
-parity_box['values'] = list(_parity.keys())
-
-stopBitVar = tk.StringVar(window)
-stopBitVar.set('One')
-stopBit_frame = tk.LabelFrame(serial_frame, text='Stop Bit')
-stopBit_frame.grid(column=0, row=4)
-stopBit_box = ttk.Combobox(stopBit_frame, text=stopBitVar, value=stopBitVar)
-stopBit_box.grid(column=0, row=0)
-stopBit_box['values'] = list(_stopBit.keys())
-
-handShakeVar = tk.StringVar(window)
-handShakeVar.set('None')
-handShake_frame = tk.LabelFrame(serial_frame, text='HandShake')
-handShake_frame.grid(column=0, row=5)
-handShake_box = ttk.Combobox(handShake_frame, text=handShakeVar, value=handShakeVar)
-handShake_box.grid(column=0, row=0)
-handShake_box['values'] = _handShake_choices
-
-# ____________________button for opening the serial port
-open_photo = tk.PhotoImage(file='open.png')
-close_photo = tk.PhotoImage(file='close.png')
-open_label = tk.Label(serial_frame, image=open_photo)
-open_label.grid(column=0, row=6, sticky='w', padx=(10, 0), pady=(10, 0))
-open_button = tk.Button(serial_frame, text='Open Port', command=open_port, width=10)
-open_button.grid(column=0, row=6, pady=(10, 0), padx=(40, 0))
-
-# ____________________label and button for clearing the receive data box details
-clear_photo = tk.PhotoImage(file='clear2.png')
-clear_Label = tk.Label(serial_frame, image=clear_photo)
-clear_Label.grid(column=0, row=7, sticky='w', padx=(10, 0), pady=(5, 0))
-clear_button = tk.Button(serial_frame, text='Clear Output',
-                         command=lambda: receive_box.delete('1.0', tk.END))
-clear_button.grid(column=0, row=7, padx=(40, 0), pady=(5, 0))
-
-# ____________________ABOUT message box button
-about_photo = tk.PhotoImage(file='about.png')
-about_label = tk.Label(serial_frame, image=about_photo)
-about_label.grid(column=0, row=8, sticky='w', padx=(10, 0), pady=(5, 0))
-about_button = tk.Button(serial_frame, text='ABOUT', foreground='blue', width=10, command=show_info)
-about_button.grid(column=0, row=8, pady=(5, 0), padx=(40, 0))
 
 if __name__ == '__main__':
-    task = Thread(target=print_result)
+    app = MySerialWindow()
+    task = Thread(target=app.print_result)
     task.setDaemon(True)
-    run()
+    app.mainloop()
