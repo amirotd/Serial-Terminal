2d17bddfd4607728e9ae1bcff5a6245cf211f0b5
amir otd <amir.otd.1@gmail.com>
2021-10-08 21:05:57 +0200

    Changed some variable names in Graph.py

--- Graph.py
+++ Graph.py
@@ -7,53 +7,47 @@ import sys
 class MyPlotWindow:
 
     def __init__(self):
-        self.data = [0]
-        self.traces = dict()
-        self.phase = 0
-        self.cnt = 0
+        self._data = [0]
+        self._cnt = 0
 
         pg.setConfigOptions(antialias=True)
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.win = pg.GraphicsWindow(title="Basic plotting example")
-        self.win.resize(1000, 600)
-        self.win.setWindowTitle('pyQtGraph example : plotting')
-        self.win.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.CustomizeWindowHint)
-        self.win.show()
-        self.canvas = self.win.addPlot(title='Serial Data')
-        self.canvas.showGrid(x=True, y=True, alpha=0.5)
-        self.canvas.setLabel('left', 'amplitude')
-        self.canvas.setLabel('bottom', 'Time')
-
-        self.traces = self.canvas.plot(pen='y')
+        self._app = QtWidgets.QApplication(sys.argv)
+        self._win = pg.GraphicsWindow(title="Plotting Window")
+        self._win.resize(1000, 600)
+        self._win.setWindowTitle('Serial Data : plotting')
+        self._win.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.CustomizeWindowHint)
+        self._win.show()
+        self._canvas = self._win.addPlot(title='Serial Data')
+        self._canvas.showGrid(x=True, y=True, alpha=0.5)
+        self._canvas.setLabel('left', 'amplitude')
+        self._canvas.setLabel('bottom', 'Time')
+        self._traces = self._canvas.plot(pen='y')
 
     @staticmethod
-    def start():
+    def _start():
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             QtWidgets.QApplication.instance().exec_()
 
-    def trace(self, dataset_y):
-        self.traces.setData(dataset_y)
-        self.canvas.setXRange(0, 1000, padding=0)
-        self.canvas.setYRange(0, 1024, padding=0)
+    def _trace(self, dataset_y):
+        self._traces.setData(dataset_y)
+        self._canvas.setXRange(0, 1000, padding=0)
+        self._canvas.setYRange(0, 1024, padding=0)
 
-    def animation(self, serdata):
+    def animation(self, running_data):
         timer = QtCore.QTimer()
-        timer.timeout.connect(lambda: self.update(serdata))
+        timer.timeout.connect(lambda: self.update(running_data))
         timer.start()
-        self.start()
+        self._start()
 
-    def update(self, inputdata):
-        # s = np.sin(2 * np.pi * self.t + self.phase)
-        # self.trace(self.t, s)
-        # self.phase += 0.1
-        self.data.append(inputdata)
-        xdata = np.array(self.data, dtype='float64')
-        self.trace(xdata)
-        self.app.processEvents()
+    def update(self, input_data):
+        self._data.append(input_data)
+        _new_data = np.array(self._data, dtype='float64')
+        self._trace(_new_data)
+        self._app.processEvents()
 
-        self.cnt = self.cnt + 1
-        if len(self.data) > 1000:
-            self.data.clear()
+        self._cnt = self._cnt + 1
+        if len(self._data) > 1000:
+            self._data.clear()
 
 
 if __name__ == '__main__':
